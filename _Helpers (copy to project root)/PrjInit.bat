8ad63364efa4ad79dc150f9759df0271dc135de8
Michael Jennings <michael.jennings@thrillux.com>
2018-01-06 03:23:53 -0800

    Fix for prjinit

--- _Helpers (copy to project root)/PrjInit.bat
+++ _Helpers (copy to project root)/PrjInit.bat
@@ -3,13 +3,16 @@
 @echo #  Requires Projeny_VR:
 @echo #  https://github.com/thrillux/Projeny_VR
 @echo # 
-@echo #  If this project is open in Unity, close it before running this.
+@echo #  Close Unity before proceeding
 @echo # 
+pause
+REM Run 2x passes to get past any issues
 @echo on
-prj --deleteAllLinks
-prj -cla --init
+call "prj.bat" --deleteAllLinks -cla --init
+call "prj.bat" --deleteAllLinks -cla --init
 @echo off
 @echo # 
 @echo #  Open a project folder in Unity. Example:
 @echo #  UnityProjects\Playground\Playground-Android
 @echo # 
+pause
